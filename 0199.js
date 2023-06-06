4ab20f019c4314bdbac755d2b372d57bdc59108e
alincode <alin.code@gmail.com>
2023-06-06 22:20:33 +0800

    update: 199. Binary Tree Right Side View

--- 0199.js
+++ 0199.js
@@ -12,27 +12,22 @@
  */
 var rightSideView = function (root) {
   if (!root) return [];
-
-  const result = [];
+  let result = [];
   const queue = [root];
 
   while (queue.length > 0) {
     const levelSize = queue.length;
     let lastNodeValue;
-
     for (let i = 0; i < levelSize; i++) {
-      const node = queue.shift();
+      let node = queue.shift();
       lastNodeValue = node.val;
       if (node.left) queue.push(node.left);
       if (node.right) queue.push(node.right);
     }
-
     result.push(lastNodeValue);
   }
-
   return result;
 };
-
 // BFS
 
 // 這個解法的時間複雜度為 O(N)，其中 N 是二叉樹的節點數量。
