8bfc71ca9a063de2d5605f61dba77bea72e009f2
alincode <alin.code@gmail.com>
2023-06-08 23:11:14 +0800

    update 62. Unique Paths

--- 0062-2.js
+++ 0062-2.js
@@ -7,16 +7,19 @@ const assert = require("assert");
  */
 var uniquePaths = function (m, n) {
   // 計算組合數 C(m+n-2, m-1)
-  const totalSteps = m + n - 2;
-  const downSteps = m - 1;
-  let result = 1;
-
-  for (let i = 1; i <= downSteps; i++) {
-    result = (result * (totalSteps - downSteps + i)) / i;
-  }
-
-  return Math.round(result);
+  const n = m + n - 2;
+  const k = m - 1;
+  return combination(n, k);
 };
+
+function combination(n, k) {
+  // 計算二項式係數
+  let result = 1;
+  for (let i = 1; i <= k; i++) {
+    result *= (n - i + 1) / i;
+  }
+  return Math.round(result);
+}
 
 assert.equal(uniquePaths(3, 7), 28);
 assert.equal(uniquePaths(3, 2), 3);
