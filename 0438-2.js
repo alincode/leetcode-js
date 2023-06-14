b47f10d4c229f6743ee9b122c17c1e2346d3f026
alincode <alin.code@gmail.com>
2023-06-14 15:06:13 +0800

    update 438. Find All Anagrams in a String

--- 0438-2.js
+++ 0438-2.js
@@ -1,52 +1,38 @@
 const assert = require("assert");
 
 /**
- * @param {string} s
- * @param {string} p
- * @return {number[]}
+ * @param {string} s - 字符串 s
+ * @param {string} p - 字符串 p
+ * @return {number[]} - 返回符合異位詞的起始索引
  */
 var findAnagrams = function (s, p) {
-  const pCount = new Array(26).fill(0); // p字串的字母計數器
-  const result = [];
+  const map = new Array(26).fill(0);
   const firstCharCode = "a".charCodeAt();
 
-  // 初始化p字串的字母計數器
-  for (let c of p) pCount[c.charCodeAt() - firstCharCode]++;
+  // 統計字符串 p 中各字符出現次數
+  for (let char of p) map[char.charCodeAt(0) - firstCharCode]++;
 
-  let left = 0; // 窗口左邊界索引
-  let right = 0; // 窗口右邊界索引
-  let count = p.length; // 窗口內剩餘需要匹配的字母數量
+  const res = [];
+  let left = 0;
 
-  // 滑動窗口
-  while (right < s.length) {
-    const charCode = s.charCodeAt(right) - firstCharCode;
+  // 遍歷字符串 s
+  for (let right = 0; right < s.length; right++) {
+    // 當前字符轉換成數字的索引
+    const sChar = s.charCodeAt(right) - firstCharCode;
+    // 將當前字符出現次數減一
+    map[sChar]--;
 
-    // 窗口右邊界字母計數減一
-    pCount[charCode]--;
-
-    // 如果窗口內的字母是p的一個異位詞，則更新count
-    if (pCount[charCode] >= 0) count--;
-
-    // 窗口大小超過p長度，移動左邊界
-    if (right - left + 1 > p.length) {
-      const leftCharCode = s.charCodeAt(left) - firstCharCode;
-
-      // 窗口左邊界字母計數加一
-      pCount[leftCharCode]++;
-
-      // 如果加一後的計數大於零，表示需要重新匹配
-      if (pCount[leftCharCode] > 0) count++;
-
-      left++; // 移動左邊界
+    // 當出現某字符次數小於 0 時，表示該字符超出了異位詞的要求，需要移動左指針
+    while (map[sChar] < 0) {
+      // 將左指針指向的字符出現次數加一，並將左指針右移
+      map[s[left++].charCodeAt(0) - firstCharCode]++;
     }
 
-    // 窗口內的字母完全匹配，添加起始索引到結果中
-    if (count === 0) result.push(left);
-
-    right++; // 移動右邊界
+    // 當窗口大小與字符串 p 長度相等時，表示找到了一個符合要求的異位詞，將左指針加入結果陣列
+    if (right - left + 1 === p.length) res.push(left);
   }
 
-  return result;
+  return res;
 };
 
 assert.deepEqual(findAnagrams("cbaebabacd", "abc"), [0, 6]);
