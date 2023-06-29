b3618c146d4fe468423140da28d720d73f3a6eb1
alincode <alin.code@gmail.com>
2023-06-29 10:53:07 +0800

    42. Trapping Rain Water

---
+++ 0042-3.js
@@ -0,0 +1,23 @@
+const assert = require("assert");
+
+var trap = function (height) {
+  let water = 0; // 雨水量
+  let stack = []; // 單調遞減棧，存儲柱子的索引
+
+  for (let i = 0; i < height.length; i++) {
+    while (stack.length && height[i] > height[stack[stack.length - 1]]) {
+      let top = stack.pop(); // 棧頂元素索引
+      if (!stack.length) break; // 棧中沒有左邊界，退出循環
+      let left = stack[stack.length - 1]; // 左邊界柱子的索引
+      let distance = i - left - 1; // 距離 = 當前柱子索引 - 左邊界柱子索引 - 1
+      let boundedHeight = Math.min(height[i], height[left]) - height[top]; // 雨水高度 = 左邊界柱子高度和當前柱子高度的最小值 - 棧頂柱子高度
+      water += distance * boundedHeight; // 雨水量 = 距離 * 雨水高度
+    }
+    stack.push(i); // 將當前柱子的索引壓入棧中
+  }
+
+  return water;
+};
+
+assert.equal(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 6);
+assert.equal(trap([4, 2, 0, 3, 2, 5]), 9);
---
+++ 0042.js
@@ -0,0 +1,33 @@
+const assert = require("assert");
+
+var trap = function (height) {
+  if (height.length === 0) return 0;
+
+  const n = height.length;
+  const leftMax = new Array(n); // 存儲每個位置左邊的最大高度
+  const rightMax = new Array(n); // 存儲每個位置右邊的最大高度
+
+  // 計算左邊的最大高度
+  leftMax[0] = height[0];
+  for (let i = 1; i < n; i++) {
+    leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+  }
+
+  // 計算右邊的最大高度
+  rightMax[n - 1] = height[n - 1];
+  for (let i = n - 2; i >= 0; i--) {
+    rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+  }
+
+  let water = 0; // 雨水量
+
+  // 遍歷每個位置，計算該位置能夠接住的雨水量
+  for (let i = 0; i < n; i++) {
+    water += Math.min(leftMax[i], rightMax[i]) - height[i];
+  }
+
+  return water;
+};
+
+assert.equal(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 6);
+assert.equal(trap([4, 2, 0, 3, 2, 5]), 9);
